a86075bc9facf681b594b5ea623b5031f8d53576
5341kmann <grant.sackmann@icloud.com>
2024-10-03 09:43:52 -0500

    Implement the  method.

--- src/main/java/edu/grinnell/csc207/util/StringUtils.java
+++ src/main/java/edu/grinnell/csc207/util/StringUtils.java
@@ -10,18 +10,36 @@ import java.io.PrintWriter;
  * 
  * @author Samuel A. Rebelsky
  * @author Your Name Here
- */ 
+ */
 public class StringUtils {
   // +------------------+--------------------------------------------
   // | Provided methods |
   // +------------------+
-    
+
   /**
    * Determine whether the parens match in string.
    */
   public static boolean checkMatching(String str) {
     Stack<Character> parens = new LinkedStack<Character>();
-    return false;       // STUB
-  } // checkMatching
-} // class StringUtils    
+    for (int i = 0; i < str.length(); i++) {
+      if (str.charAt(i) == '(' || str.charAt(i) == '[') {
+        try {
+          parens.push(str.charAt(i));
+        } catch (Exception e) {
+        } //try/catch
+      } // if
+      if (str.charAt(i) == ')' || str.charAt(i) == ']') {
+        try {
+          char recent = parens.pop();
+          if ((recent == '(' && str.charAt(i) == ')') || (recent == '[' && str.charAt(i) == ']')) {
+            return true;
+          } // if
+        } catch (Exception e) {
+        } //try/catch
+      }
+    } // for
+    return false;
+  } //checkMatching(String)
+}
+// class StringUtils
 
